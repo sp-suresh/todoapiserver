41f2a220f701311f3db52dbb6bd8e78445f942d6
Suresh Prajapati <sureshpraja1234@gmail.com>
2019-10-09 12:32:07 +0530

    Update mongoCollSetup.js

--- scripts/mongoCollSetup.js
+++ scripts/mongoCollSetup.js
@@ -18,8 +18,8 @@ db.users.insertMany([
     'lnm': 'Prajapati',
     'hashSalt': '$2b$10$U/ka1Lh1tqJAJsWRo0zMVu',
     'pwdHash': '$2b$10$U/ka1Lh1tqJAJsWRo0zMVuxdTK3OkvRwziaieohqpBqUaIikJWwTK',//password
-    'eml': 'sureshpraja1234@gmail.com',
-    'mob': '9004794642'
+    'eml': 'sureshpraja1234@test.com',
+    'mob': '7788994455'
   },
   {
     'idx': 2,
@@ -27,7 +27,7 @@ db.users.insertMany([
     'lnm': 'Prajapati',
     'hashSalt': '$2b$10$U4KJ8MocuSARxaba0/cy4O',
     'pwdHash': '$2b$10$U4KJ8MocuSARxaba0/cy4Orru4rZe12Lh48X/6PoNEUYOAimauOse',//password2
-    'eml': 'suresh.prajapati@zebpay.com',
-    'mob': '9869547580'
+    'eml': 'suresh.prajapati@z.com',
+    'mob': '1122558877'
   }
 ]);
